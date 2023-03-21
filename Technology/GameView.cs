5f3951b522f806a56b62b4387949a1003b5f6eea
Сергей Полевой <sergpolevoi@yandex.ru>
2023-03-22 02:05:07 +0400

    Готово

---
+++ Technology/GameView.cs
@@ -0,0 +1,9 @@
+﻿namespace Technology
+{
+    public class GameView
+    {
+        public int id { get; set; }
+        public string image { get; set; }
+        public string name { get; set; }
+    }
+}
--- Technology/MainWindow.xaml.cs
+++ Technology/MainWindow.xaml.cs
@@ -1,28 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Technology
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<GameView> Shop { get; set; }
+        public ObservableCollection<GameView> Library { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            Shop = new ObservableCollection<GameView>()
+            {
+                new GameView() { id = 1, name = "The Sims", image = "/Resources/The_Sims_Coverart-250x300.png"},
+                new GameView() { id = 2, name = "Resident Evil 5", image = "/Resources/resident_evil.jpg"},
+                new GameView() { id = 3, name = "Resident Evi: Operation Raccoon City", image = "/Resources/Resident_Evil_ORC_Cover.png"}
+            };
+            Library = new ObservableCollection<GameView>()
+            {
+                new GameView() { id = 4, name = "Warcraft III: Reign of Chaos", image = "/Resources/Front-Cover-WC3-Reign-of-Chaos-Night-Elf.jpg"},
+                new GameView() { id = 5, name = "Magical Chase", image = "/Resources/magical_chase.jpg"},
+                new GameView() { id = 6, name = "Battle Realms", image = "/Resources/BRCover.jpg"}
+            };
+            gamesList.ItemsSource = Shop;
+        }
+
+        private void enterShop(object sender, ExecutedRoutedEventArgs e)
+        {
+            gamesList.ItemsSource = Shop;
+        }
+
+        private void enterLibrary(object sender, ExecutedRoutedEventArgs e)
+        {
+            gamesList.ItemsSource = Library;
         }
     }
 }
